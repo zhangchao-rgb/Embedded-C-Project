0816baf73e4522df8697de44b36928ba16b309a6
zhangchao-rgb <82945742+zhangchao-rgb@users.noreply.github.com>
2021-04-21 23:56:11 +0800

    Add files via upload

---
+++ contact/src/contact.c
@@ -0,0 +1,172 @@
+﻿//头文件
+#include "contact.h"
+
+void InitContact(contact_struct *ps)
+{
+	memset(ps->data,0,sizeof(ps->data));
+	ps->size = 0;	//设置通信录最初只有0个元素
+}
+
+void AddContact(contact_struct *ps)
+{
+	if (ps->size == MAX)
+	{
+		printf("通信录已满,无法增加!\n");
+	}
+	else
+	{
+		printf("请输入名字:>");
+		scanf("%s",ps->data[ps->size].name);
+		printf("请输入年龄:>");
+		scanf("%d",&(ps->data[ps->size].age));
+		printf("请输入性别:>");
+		scanf("%s",ps->data[ps->size].sex);
+		printf("请输入电话:>");
+		scanf("%s",ps->data[ps->size].tele);
+		printf("请输入住址:>");
+		scanf("%s",ps->data[ps->size].addr);
+
+		ps->size++;
+		printf("添加成功!\n");
+					
+	}
+}
+
+void ShowContact(const contact_struct *ps)
+{
+	if (ps->size == 0)
+	{
+		printf("通信录为空\n");
+	}
+	else
+	{
+		int i;
+		printf("%-20s\t%-4s\t%-5s\t%-12s\t%-20s\n","姓名","年龄","性别","电话","住址");
+		for (i = 0; i < ps->size; i++)
+		{
+			printf("%-20s\t%-4d\t%-5s\t%-12s\t%-20s\n",
+				ps->data[i].name,
+				ps->data[i].age,
+				ps->data[i].sex,
+				ps->data[i].tele,
+				ps->data[i].addr);
+		}
+	}
+	
+}
+
+static int FindByName(const contact_struct *ps,char name[MAX_NAME])
+{
+	int i;
+	for (i = 0; i < ps->size; i++)
+	{
+		if(0 == strcmp(ps->data[i].name,name))
+		{
+			return i;
+		}
+	}
+
+	return -1;	//找不到的情况
+}
+
+void DelContact(contact_struct *ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要删除人的名字:>");
+	scanf("%s",name);
+
+	//1.找人,找到返回元素下标，找不到返回-1
+	int pos = FindByName(ps,name);
+	if(-1 == pos)
+	{
+		printf("要删除的人不存在!\n");
+	}
+	//2.删除
+	else
+	{
+		int j = 0;
+		for (j = pos; j < ps->size-1; j++)
+		{
+			ps->data[j] = ps->data[j+1];
+		}
+		ps->size--;
+		printf("删除成功!\n");
+	}
+	
+}
+
+void SearchContact(const contact_struct *ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要查找人的名字:>");
+	scanf("%s",name);
+	int pos = FindByName(ps,name);
+	if(-1 == pos)
+	{
+		printf("要查找的人不存在!\n");
+	}
+	else
+	{
+		printf("%-20s\t%-4s\t%-5s\t%-12s\t%-20s","姓名","年龄","性别","电话","住址");
+		printf("%-20s\t%-4d\t%-5s\t%-12s\t%-20s",
+				ps->data[pos].name,
+				ps->data[pos].age,
+				ps->data[pos].sex,
+				ps->data[pos].tele,
+				ps->data[pos].addr);
+	}
+
+}
+
+void ModifyContact(contact_struct *ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要修改人的名字:>");
+	scanf("%s",name);
+	int pos = FindByName(ps,name);
+	if(-1 == pos)
+	{
+		printf("要修改的人不存在!\n");
+	}
+	else
+	{
+	
+		printf("请输入名字:>");
+		scanf("%s",ps->data[pos].name);
+		printf("请输入年龄:>");
+		scanf("%d",&(ps->data[pos].age));
+		printf("请输入性别:>");
+		scanf("%s",ps->data[pos].sex);
+		printf("请输入电话:>");
+		scanf("%s",ps->data[pos].tele);
+		printf("请输入住址:>");
+		scanf("%s",ps->data[pos].addr);
+
+		printf("修改成功!\n");
+		
+	}
+
+}
+
+void SortContact(contact_struct *ps)
+{
+	peoInfo_struct test;
+	int i,j;
+	for(i=0;i<ps->size;i++)
+    {
+       for(j=0;j<ps->size-i-1;j++)
+        {
+            //if(ps->data[j].age<ps->data[j+1].age)				//根据单一数据项，对整个结构体进行排序
+			if(strcmp(ps->data[j].name, ps->data[j+1].name) > 0)//根据单一数据项，对整个结构体进行排序
+            {
+                test=ps->data[j+1];				//结构体允许整体赋值
+                ps->data[j+1]=ps->data[j];
+                ps->data[j]=test;
+            }
+        }
+    }
+	
+	printf("排序成功!\n");
+}
+
+
---
+++ contact/src/test.c
@@ -0,0 +1,99 @@
+﻿//头文件
+#include "contact.h"
+
+//菜单列表
+void menu(void)
+{
+	printf("*******************************\n");
+	printf("******** 1. add   2.del********\n");
+	printf("******** 3.search 4.modify*****\n");
+	printf("******** 5.show	  6.sort*******\n");
+	printf("******** 0.exit         *******\n");
+	printf("*******************************\n");
+
+}
+
+int main(int argc,char *argv[])
+{
+	int input;
+	//创建通信录
+	contact_struct con;	//con就是通信录，里面包含1000个元素
+	//初始化通信录
+	InitContact(&con);
+
+	while(1)
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d",&input);
+		switch (input)
+		{
+			case ADD:
+				AddContact(&con);
+				break;
+			case DEL:
+				DelContact(&con);
+				break;	
+			case SEARCH:
+				SearchContact(&con);
+				break;
+			case MODIFY:
+				ModifyContact(&con);
+				break;
+			case SHOW:
+				ShowContact(&con);
+				break;
+			case SORT:
+				SortContact(&con);
+				break;
+			case EXIT:
+				printf("退出通信录!\n");
+				return -1;
+			default:
+				printf("选择错误!\n");
+				break;						
+		}
+	
+	}
+	
+/*
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d",&input);
+		switch (input)
+		{
+			case ADD:
+				AddContact(&con);
+				break;
+			case DEL:
+				DelContact(&con);
+				break;	
+			case SEARCH:
+				SearchContact(&con);
+				break;
+			case MODIFY:
+				ModifyContact(&con);
+				break;
+			case SHOW:
+				ShowContact(&con);
+				break;
+			case SORT:
+				SortContact(&con);
+				break;
+			case EXIT:
+				printf("退出通信录!\n");
+				break;
+			default:
+				printf("选择错误!\n");
+				break;						
+		}
+	
+	}
+	while (input);
+*/	
+	return 0;
+}
+
+
